9e289e8265a261a7bf61ef4b3827fab0d02e37a8
sofiamv <league@WTS21.attlocal.net>
2018-08-18 15:57:54 -0700

    Step 12 part 9
    ggftkjuyrvffgegrgffgeeggrgtrgrge

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -8,82 +8,93 @@ public class ObjectManager {
 	Rocketship rocket;
 	long enemyTimer = 0;
 	int enemySpawnTime = 1000;
-	ObjectManager(Rocketship rocket){
+
+	ObjectManager(Rocketship rocket) {
 		this.rocket = rocket;
 	}
-public void update(){
-	rocket.update();
-	for(int i = 0; i < ProjectileObjects.size(); i++){
-		ProjectileObjects.get(i).update();
-	}
-	for(int i = 0; i < AlienObjects.size(); i++){
-		AlienObjects.get(i).update();
-	}
-	
-	
-	
-}
-public void draw(Graphics graphic){
-	rocket.draw(graphic);
-	for(int i = 0; i < ProjectileObjects.size(); i++){
-		ProjectileObjects.get(i).draw(graphic);
-	}for(int i = 0; i < AlienObjects.size(); i++){
-		AlienObjects.get(i).draw(graphic);
-	}
-	
-	
-}
-public void addProjectile(Projectile a){
-	ProjectileObjects.add(a);
-}
-public void addAlien(Alien b) {
-	AlienObjects.add(b);
-}
-public void ManageEnemies() {
-	 if(System.currentTimeMillis() - enemyTimer >= enemySpawnTime){
-         addAlien(new Alien(new Random().nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
-enemyTimer = System.currentTimeMillis();
- }
 
-}
-public void purgeObjects() {
-	for(int i = 0; i < AlienObjects.size(); i++) {
-		if(AlienObjects.get(i).y < 0) {
-			AlienObjects.remove(AlienObjects.get(i));}}
-		for(int i = 0; i < AlienObjects.size(); i++) {
-		if(AlienObjects.get(i).isAlive==false) {
-			AlienObjects.remove(i);
-		}	}
-	
-		for(int i = 0; i < ProjectileObjects.size(); i++) {
-			if(ProjectileObjects.get(i).y < 0) {
-				ProjectileObjects.remove(ProjectileObjects.get(i));}
+	public void update() {
+		rocket.update();
+		for (int i = 0; i < ProjectileObjects.size(); i++) {
+			ProjectileObjects.get(i).update();
 		}
-		for(int i = 0; i < ProjectileObjects.size(); i++) {
-			if(ProjectileObjects.get(i).isAlive==false) {
+		for (int i = 0; i < AlienObjects.size(); i++) {
+			AlienObjects.get(i).update();
+		}
+
+	}
+
+	public void draw(Graphics graphic) {
+		rocket.draw(graphic);
+		for (int i = 0; i < ProjectileObjects.size(); i++) {
+			ProjectileObjects.get(i).draw(graphic);
+		}
+		for (int i = 0; i < AlienObjects.size(); i++) {
+			AlienObjects.get(i).draw(graphic);
+		}
+
+	}
+
+	public void addProjectile(Projectile a) {
+		ProjectileObjects.add(a);
+	}
+
+	public void addAlien(Alien b) {
+		AlienObjects.add(b);
+	}
+
+	public void ManageEnemies() {
+		if (System.currentTimeMillis() - enemyTimer >= enemySpawnTime) {
+			addAlien(new Alien(new Random().nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
+			enemyTimer = System.currentTimeMillis();
+		}
+
+	}
+
+	public void purgeObjects() {
+		for (int i = 0; i < AlienObjects.size(); i++) {
+			if (AlienObjects.get(i).y < 0) {
+				AlienObjects.remove(AlienObjects.get(i));
+			}
+		}
+		for (int i = 0; i < AlienObjects.size(); i++) {
+			if (AlienObjects.get(i).isAlive == false) {
+				AlienObjects.remove(i);
+			}
+		}
+		if(rocket.isAlive==false) {
+			currentState == END_STATE;
+		}
+
+		for (int i = 0; i < ProjectileObjects.size(); i++) {
+			if (ProjectileObjects.get(i).y < 0) {
 				ProjectileObjects.remove(ProjectileObjects.get(i));
-			}}
+			}
 		}
-		
-public void checkCollision() {
-	for(Alien a : AlienObjects){
-        if(rocket.collisionbox.intersects(a.collisionbox)){
-                rocket.isAlive = false;
+		for (int i = 0; i < ProjectileObjects.size(); i++) {
+			if (ProjectileObjects.get(i).isAlive == false) {
+				ProjectileObjects.remove(ProjectileObjects.get(i));
+			}
+		}
+	}
 
-        }
-        for(int i = 0; i < AlienObjects.size(); i++) {
-        	for(int i1 = 0; i1 < ProjectileObjects.size(); i1 ++) {
-        		if(AlienObjects.get(i).collisionbox.intersects(ProjectileObjects.get(i1).collisionbox)) {
-        			AlienObjects.get(i).isAlive = false;
-        			ProjectileObjects.get(i1).isAlive = false;
-        			
-        		}
-        	}
-        }
-     
+	public void checkCollision() {
+		for (Alien a : AlienObjects) {
+			if (rocket.collisionbox.intersects(a.collisionbox)) {
+				rocket.isAlive = false;
+
+			}
+			for (int i = 0; i < AlienObjects.size(); i++) {
+				for (int i1 = 0; i1 < ProjectileObjects.size(); i1++) {
+					if (AlienObjects.get(i).collisionbox.intersects(ProjectileObjects.get(i1).collisionbox)) {
+						AlienObjects.get(i).isAlive = false;
+						ProjectileObjects.get(i1).isAlive = false;
+
+					}
+				}
+			}
+
+		}
+	}
 
 }
-}
-
-}
-
